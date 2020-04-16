e12eb7ae64b7698d8d15193e024c99199f764ae5
Shrutisingla1995 <shrutis@simplifyworkforce.com>
2020-04-16 19:29:35 +0530

    family tree

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+import { AngularFireModule } from '@angular/fire';
+import * as firebase from 'firebase';
+
 export const environment = {
-  production: true
+  production: false,
+  firebase
 };
+firebase.initializeApp( {
+  apiKey: ' AIzaSyDRlz0j_ZLJps2_ox96EOjoj_lta-Wsgjc ',
+  authDomain: 'familytree-860e8.firebaseapp.com',
+  databaseURL: 'https://familytree-860e8.firebaseio.com',
+  projectId: 'familytree-860e8',
+  storageBucket: 'familytree-860e8.appspot.com',
+  messagingSenderId: '294385887452'
+})
