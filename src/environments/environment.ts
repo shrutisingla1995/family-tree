1947087d6782545bb323b1abffc0e7bb5edb3479
Shrutisingla1995 <shrutis@simplifyworkforce.com>
2020-04-20 11:48:20 +0530

    fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,4 @@ export const environment = {
   production: false,
   firebase
 };
-firebase.initializeApp( {
-  apiKey: ' AIzaSyDRlz0j_ZLJps2_ox96EOjoj_lta-Wsgjc ',
-  authDomain: 'familytree-860e8.firebaseapp.com',
-  databaseURL: 'https://familytree-860e8.firebaseio.com',
-  projectId: 'familytree-860e8',
-  storageBucket: 'familytree-860e8.appspot.com',
-  messagingSenderId: '294385887452'
-})
+
